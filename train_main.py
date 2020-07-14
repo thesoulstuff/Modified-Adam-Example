109f34cbfcfd04fc26a71ac8aebb6f5de7a0d868
Jose Toro <jose.toro@dataciencia.com>
2020-07-14 00:01:08 -0400

    added simple backpropagation

--- train_main.py
+++ train_main.py
@@ -7,20 +7,52 @@ def sigmoid(x):
 
 def mlp_foward(xe, W):
     activations = [xe.copy()]
+    zs = []
     for i in range(len(W)-1):
-        z = W[i].dot(activations[-1])
-        a = sigmoid(z)
+        zs.append(W[i].dot(activations[-1]))
+        a = sigmoid(zs[-1])
         activations.append(a)
     z = W[-1].dot(activations[-1] )
-    activations.append(z)
-    return activations
+    zs.append(z)
+    activations.append(zs[-1])
+    return activations, zs
 
+def derivate_sigmoid(x):
+    s = sigmoid(x)
+    return s*(1 -s)
+
+def mlp_bp_grad(act,argList):
+    e, W, z_s = argList
+    e = e*-1
+    dw_output = e
+    g_wh = []
+    deltas = [None]*len(W)
+    deltas[-1] = e*1
+    g_wh.append(dw_output)
+    dw = []
+    start = len(W)
+
+    for i in reversed(range(len(deltas)-1)):
+        deltas[i] = W[i+1].T.dot(deltas[i+1])*derivate_sigmoid(z_s[i])
+
+    dw = [d.dot(act[i].T) for i,d in enumerate(deltas)]
+
+    return dw
+
+
+def mlp_upd_gAdam(g_Wh, arglist):
+    W, mu, b1, b2, epoch = arglist
+    mu = 0.1
+    for i in range(len(g_Wh)):
+        W[i] = W[i]-mu*g_Wh[i]
+    return W
 
 
 def mlp_train(xe, ye, arglist):
     C, epochs, mu, b1, b2, hidden_sizes = arglist
     W = []
     input_size = xe.shape[0]
+    costs = []
     for size in hidden_sizes:
         r = np.sqrt(6/(size+input_size))
         W.append(np.random.uniform(
@@ -28,16 +60,25 @@ def mlp_train(xe, ye, arglist):
                 ))
         input_size = size
     r = np.sqrt(6/(1+input_size))
-    W.append(np.random.uniform(
-                low=-r, high=r, size=(1,input_size
-        ))
+    W.append(
+            np.random.uniform(
+                low=-r, high=r, size=(1,input_size)
+                )
     )
 
     for epoch in range(epochs):
-        act = mlp_foward(xe, W)
+        act, z_s = mlp_foward(xe, W)
         E = ye - act[-1]
         cost = np.square(E).mean()
+        costs.append(cost)
         print(cost)
+        args = (E, W, z_s)
+        g_Wh = mlp_bp_grad(act, args)
+        args = (W, mu, b1, b2, epoch)
+        
+        Whidden = mlp_upd_gAdam(g_Wh, args)
+        W = Whidden
+
     pass
 
 
